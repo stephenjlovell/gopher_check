2eae5a16223e03a8d47404dffef64a360de2b3bd
Stephen Lovell <sjlovell34@gmail.com>
2017-01-18 22:49:48 -0500

    move license terms to separate file

--- repetitions.go
+++ repetitions.go
@@ -2,30 +2,10 @@
 // ♛ GopherCheck ♛
 // Copyright © 2014 Stephen J. Lovell
 //-----------------------------------------------------------------------------------
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to
-// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//-----------------------------------------------------------------------------------
 
 package main
 
-import (
 // "fmt"
-)
 
 func (stk Stack) IsRepetition(ply int, halfmove_clock uint8) bool {
 	hash_key := stk[ply].hash_key
@@ -36,7 +16,6 @@ func (stk Stack) IsRepetition(ply int, halfmove_clock uint8) bool {
 		if stk[ply-2].hash_key == hash_key {
 			repetition_count += 1
 			if repetition_count == 2 {
-				// fmt.Printf("repetition found ")
 				return true
 			}
 		}
--- selector.go
+++ selector.go
@@ -2,24 +2,6 @@
 // ♛ GopherCheck ♛
 // Copyright © 2014 Stephen J. Lovell
 //-----------------------------------------------------------------------------------
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to
-// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//-----------------------------------------------------------------------------------
 
 package main
 
@@ -33,8 +15,6 @@ package main
 // 2. Non-captures that give check via get_checks().
 
 import "sync"
-
-// "fmt"
 
 const (
 	STAGE_FIRST = iota
@@ -63,10 +43,6 @@ type AbstractSelector struct {
 	this_stk        *StackItem
 	htable          *HistoryTable
 }
-
-// func (s *AbstractSelector) allocate() MoveList {
-// 	return recycler.AttemptReuse()
-// }
 
 func (s *AbstractSelector) CurrentStage() int {
 	return s.stage - 1
